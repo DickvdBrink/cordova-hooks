641fc898b94608bc2bb35c106558a10f88cfbc47
Dick van den Brink <d_vandenbrink@live.com>
2013-10-25 12:12:46 +0200

    Added hook for removing TypeScript files. This way the .ts files won't be part of the csproject and won't be packaged when creating a build.

---
+++ hooks/pre_package/deleteTypeScript.js
@@ -0,0 +1,44 @@
+/*
+ * Remove all .ts files from the platforms directory. Which are copied here by cordova prepare.
+ * Copy this file in the .cordova/hooks/pre_package for Windows Phone or the project file will be generated with the content tags for the typescript files.
+ * Doesnt work with cordova 3.1, because it doesn't have the pre_package hook.
+ */
+
+var fs = require('fs');
+var path = require('path');
+var cordovaUtil = require('cordova/src/util');
+var cordovaPlatforms = require('cordova/platforms');
+
+var walk = function(dir, extensions) {
+    var results = [];
+    var list = fs.readdirSync(dir);
+    list.forEach(function(file) {
+        file = path.join(dir, file);
+        var stat = fs.statSync(file);
+        // Check given directory recursive.
+        if (stat && stat.isDirectory()) {
+            results = results.concat(walk(file, extensions));
+        } else {
+            var extension = path.extname(file);
+            if (extension === '' || !Array.isArray(extensions) || extensions.indexOf(extension.substring(1)) > -1) {
+                results.push(file);
+            }
+        }
+    });
+    return results;
+}
+
+var platforms = cordovaUtil.listPlatforms('.');
+
+platforms.forEach(function(platform) {
+    if (cordovaPlatforms[platform]) {
+        var projectPath = path.resolve(path.join('platforms', platform));
+        var parser = new cordovaPlatforms[platform].parser(projectPath);
+        var filesToRemove = walk(parser.www_dir(), ['ts']);
+        filesToRemove.forEach(function(file) {
+            fs.unlinkSync(file);
+        });
+    } else {
+        console.log('Platform unsupported: ' + platform)
+    }
+});
